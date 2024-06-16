663b08a98240758d20d05c11e7dda2ad6d178466
Shivam Rajput <raj.shiviraj@gmail.com>
2024-06-16 16:59:04 +0530

    Shivi | update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+
 rm -rf node_modules
 npm install && \
-npm run build --omit=dev && \
-rm -rf node_modules && \
-npm install --production --ignore-scripts --prefer-offline && \
+npm run build && \
 docker buildx build --no-cache --platform=linux/arm64,linux/amd64 -t shiviraj/robotutor-tech-admin:latest --push .
