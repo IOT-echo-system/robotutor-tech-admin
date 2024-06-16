44c3a16aea31ba80024710cb96500b76a21ffcaf
Shivam Rajput <raj.shiviraj@gmail.com>
2024-06-16 17:12:35 +0530

    Shivi | update dockerfile and build.sh

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:20.11-alpine
 WORKDIR /app
 ENV NODE_ENV production
 ENV PORT 1337
+RUN npm run build
 COPY . .
 EXPOSE 1337
 CMD ["npm", "start"]
--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-rm -rf node_modules
-npm install && \
-npm run build && \
 docker buildx build --no-cache --platform=linux/arm64,linux/amd64 -t shiviraj/robotutor-tech-admin:latest --push .
