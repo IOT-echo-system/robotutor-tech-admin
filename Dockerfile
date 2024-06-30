1048a196bc16e3188098908dc32bf80b43132141
Shivam Rajput <raj.shiviraj@gmail.com>
2024-06-30 23:10:40 +0530

    Shivi | update build script.

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,5 @@ WORKDIR /app
 ENV NODE_ENV production
 ENV PORT 1337
 COPY . .
-RUN rm -rf node_modules
-RUN npm install
-RUN npm run build
 EXPOSE 1337
 CMD ["npm", "start"]
--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker buildx build --no-cache --platform=linux/arm64,linux/amd64 -t shiviraj/robotutor-tech-admin:latest --push .
+rm -rf node_modules
+npm install --platform=linuxmusl --arch=x64 sharp
+npm run build
+docker buildx build --no-cache --platform=linux/amd64 -t shiviraj/robotutor-tech-admin:latest --push .
+
+rm -rf node_modules
+npm install --platform=linuxmusl --arch=arm64 sharp
+npm run build
+docker buildx build --no-cache --platform=linux/arm64 -t shiviraj/robotutor-tech-admin:aarm64 --push .
